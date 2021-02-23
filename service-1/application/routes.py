cbfc6ea72a433c172c77bdc4d0e6518fcefe5fb6
subeca <subeca97@gmail.com>
2021-02-23 16:06:44 +0000

    more files created

--- service-1/application/routes.py
+++ service-1/application/routes.py
@@ -6,15 +6,7 @@ import requests
 
 @app.route('/')
 def index():  
-    car_make = requests.get("http://service2-backend:5000/car_name")
-    car_colour = requests.get("http://service3-backend:5000/colour")
-    price = requests.post("http://service4-backend:5000/price", json={'car_make':car_make.text, 'car_colour':car_colour.text})
-    
-    car = Cars(car_manufacturer = car_make.text, car_colour = car_colour.text, price=price.text)
-    
-    db.session.add(car)
-    db.session.commit()
-    cars = Cars.query.order_by(Cars.id.desc()).limit(5)
+  
     
     return render_template("index.html", car_name=car_make.text, colour=car_colour.text, cars=cars, price=price.text)
 
