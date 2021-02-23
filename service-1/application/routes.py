89abc801359f1af86dcf9dbe4b1aada706f2c8f2
subeca <77278616+subeca@users.noreply.github.com>
2021-02-23 16:10:31 +0000

    Update routes.py

--- service-1/application/routes.py
+++ service-1/application/routes.py
@@ -1,5 +1,5 @@
 from application import app, db
-from application.models import Cars
+from application.models import Houses
 from flask import render_template
 from sqlalchemy import desc
 import requests
